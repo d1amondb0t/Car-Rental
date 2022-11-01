e945eb1f2519888d7ce83eaa90d65ce7588f2dc8
d1amondb0t <d1amondb0t@outlook.com>
2022-11-01 16:24:24 -0400

    Modification au schéma UML

--- CarRentalAPI/CarRentalAPI/Model/Car.cs
+++ CarRentalAPI/CarRentalAPI/Model/Car.cs
@@ -3,17 +3,10 @@ namespace CarRentalAPI.Model
 {
 	public class Car
 	{
-		public Car()
-		{
-		}
-
 		public int Id { get; set; }
-
+		public string Name { get; set; }
 		public string Model { get; set; }
-
 		public string Year { get; set; }
-
-		public string Mark { get; set; }
 	}
 }
 
--- CarRentalAPI/CarRentalAPI/Model/Rental.cs
+++ CarRentalAPI/CarRentalAPI/Model/Rental.cs
@@ -3,19 +3,15 @@ namespace CarRentalAPI.Model
 {
 	public class Rental
 	{
-		public Rental()
-		{
-		}
-
 		public int Id { get; set; }
 
-		public string Username { get; set; }
+		public User User { get; set; }
+		public Car Car { get; set; }
+		public float Price { get; set; }
+		public  string Description { get; set; }
+		public string Location { get; set; }
+		public DateTime Availability { get; set; }
 
-		public string Email { get; set; }
-
-		public string Password { get; set; }
-
-		public string Password2 { get; set; }
 	}
 }
 
